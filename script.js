354bfd360b21ae7ff372eed0f9c68334b2bb5cab
Chris Wu <cwu0569@gmail.com>
2020-07-20 22:52:51 -0700

    added random color feature

--- script.js
+++ script.js
@@ -4,6 +4,7 @@
 const grid = document.querySelector('.grid');
 const clearBtn = document.getElementById('clear-btn');
 const resizeBtn = document.getElementById('resize-btn');
+const randomBtn = document.getElementById('random-btn');
 
 let boxArr = [];
 
@@ -24,11 +25,27 @@ function createGrid(dimension = 16) {
 function changeColor(event) {
     const id = event.target.id;
     const divBox = document.getElementById(`${id}`);
-    divBox.classList.add('change-color');
+    divBox.style.backgroundColor = '#2d2d2d';
+}
+
+function changeRandomColor(event) {
+    const id = event.target.id;
+    const divBox = document.getElementById(`${id}`);
+    const red = Math.floor(Math.random() * 256);
+    const green = Math.floor(Math.random() * 256);
+    const blue = Math.floor(Math.random() * 256);
+
+    divBox.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
+}
+
+function createRandomGrid() {
+    boxArr.forEach(cur => cur.addEventListener('mouseover', changeRandomColor));
 }
 
 function clearGrid() {
-    boxArr.forEach(cur => cur.classList.remove('change-color'));
+    boxArr.forEach(cur => cur.style.backgroundColor = 'whitesmoke');
+    boxArr.forEach(cur => cur.removeEventListener('mouseover', changeRandomColor));
+    boxArr.forEach(cur => cur.addEventListener('mouseover', changeColor));
 }
 
 function changeDimensions() {
@@ -48,3 +65,4 @@ function changeDimensions() {
 window.addEventListener('load', createGrid());
 clearBtn.addEventListener('click', clearGrid);
 resizeBtn.addEventListener('click', changeDimensions);
+randomBtn.addEventListener('click', createRandomGrid);
