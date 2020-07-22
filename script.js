c0c695a0b8b4cf74bdbf03ceb9000ed1ac6e65c4
Chris Wu <cwu0569@gmail.com>
2020-07-21 22:02:00 -0700

    add shaded button feature and random button styling

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-
-
 // DOM objects
 const grid = document.querySelector('.grid');
 const clearBtn = document.getElementById('clear-btn');
 const resizeBtn = document.getElementById('resize-btn');
 const randomBtn = document.getElementById('random-btn');
+const shadedBtn = document.getElementById('shaded-btn');
 
 let boxArr = [];
+let shadedFlag = true;
 let randomFlag = true;
 
 function createGrid(dimension = 16) {
@@ -15,6 +15,7 @@ function createGrid(dimension = 16) {
     for (let i = 0; i < (dimension * dimension); i++) {
         const box = document.createElement('div');
         box.setAttribute('id',`box${i}`);
+        box.setAttribute('data-count', '0');
         box.classList.add('default-color');
         boxArr.push(box);
         grid.appendChild(box);
@@ -26,6 +27,7 @@ function createGrid(dimension = 16) {
 function changeColor(event) {
     const id = event.target.id;
     const divBox = document.getElementById(`${id}`);
+
     divBox.style.backgroundColor = '#2d2d2d';
 }
 
@@ -39,36 +41,72 @@ function changeRandomColor(event) {
     divBox.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
 }
 
+function changeShadedColor(event) {
+    const id = event.target.id;
+    const divBox = document.getElementById(`${id}`);
+
+    let count = parseFloat(divBox.dataset.count);
+    if (count <= 0.9) {
+        count += 0.1;
+        divBox.style.backgroundColor = `rgba(45, 45, 45, ${count})`;
+        divBox.setAttribute('data-count', `${count}`);
+    }
+}
+
+function createShadedGrid(flag) {
+    boxArr.forEach(cur => cur.removeEventListener('mouseover', changeRandomColor));
+    randomBtn.classList.remove('rainbow-text');
+
+    flag = shadedFlag;
+    if (flag) {
+        boxArr.forEach(cur => cur.addEventListener('mouseover', changeShadedColor));
+        shadedBtn.classList.add('shaded-btn-focus');
+        shadedFlag = false;
+    } else {
+        boxArr.forEach(cur => cur.removeEventListener('mouseover', changeShadedColor));
+        boxArr.forEach(cur => cur.addEventListener('mouseover', changeColor));
+        shadedBtn.classList.remove('shaded-btn-focus');
+        shadedFlag = true;
+    }
+}
+
 function createRandomGrid(flag) {
+    boxArr.forEach(cur => cur.removeEventListener('mouseover', changeShadedColor));
+    shadedBtn.classList.remove('shaded-btn-focus');
+    
     flag = randomFlag;
     if (flag) {
         boxArr.forEach(cur => cur.addEventListener('mouseover', changeRandomColor));
-        randomBtn.classList.add('random-btn-focus');
+        randomBtn.classList.add('rainbow-text');
         randomFlag = false;
     } else {
         boxArr.forEach(cur => cur.removeEventListener('mouseover', changeRandomColor));
         boxArr.forEach(cur => cur.addEventListener('mouseover', changeColor));
-        randomBtn.classList.remove('random-btn-focus');
+        randomBtn.classList.remove('rainbow-text');
         randomFlag = true;
     }
 }
 
 function clearGrid() {
-    randomBtn.classList.remove('random-btn-focus');
-    boxArr.forEach(cur => cur.style.backgroundColor = 'whitesmoke');
+    randomBtn.classList.remove('rainbow-text');
+    shadedBtn.classList.remove('shaded-btn-focus');
     boxArr.forEach(cur => cur.removeEventListener('mouseover', changeRandomColor));
+    boxArr.forEach(cur => cur.removeEventListener('mouseover', changeShadedColor));
     boxArr.forEach(cur => cur.addEventListener('mouseover', changeColor));
+    boxArr.forEach(cur => cur.setAttribute('data-count', '0'));
+    boxArr.forEach(cur => cur.style.backgroundColor = 'whitesmoke'); 
 }
 
 function changeDimensions() {
     const input = parseInt(prompt('How many squares per side do you want?'), 10);
     if (input && typeof input === 'number') {
-        dimension = input;
-
+        randomBtn.classList.remove('rainbow-text');
+        shadedBtn.classList.remove('shaded-btn-focus');
         grid.innerHTML = '';
         boxArr = [];
+
+        dimension = input;
         createGrid(dimension);
-        randomBtn.classList.remove('random-btn-focus');
     } else {
         alert('Please enter a valid number');
         changeDimensions();
@@ -78,4 +116,5 @@ function changeDimensions() {
 window.addEventListener('load', createGrid());
 clearBtn.addEventListener('click', clearGrid);
 resizeBtn.addEventListener('click', changeDimensions);
+shadedBtn.addEventListener('click', createShadedGrid);
 randomBtn.addEventListener('click', createRandomGrid);
